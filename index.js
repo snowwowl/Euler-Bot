a2a6c689ccc295378955d9573387299cd1e48b9e
Ankit Kumar <53820439+iamankitkumar08062001@users.noreply.github.com>
2020-09-09 19:48:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,29 +33,68 @@ bot.on('message', function(msg) {
   let args = parser(msg.content);
 
   switch(args[0]) {
-    case "ping" : createDatabase();
+    case ".ping" : createDatabase(msg);
     break;
 
-    case ".add" : add(args);
+    case ".add" : add(msg, args);
+    break;
+
+    case ".display" : display(msg); 
     break;
   }
   
 });
 
 function parser(content) {
-  let array = content.split(" ");
-  return array;
+    let array = content.split(" ");
+    return array;
 }
 
-function createDatabase() {
-  msg.reply("Your ID is: " + msg.author.id)
-  db.get('users').push({id: msg.author.id, items: []}).write();
-  msg.reply("Created database");
+function createDatabase(msg) {
+    db.get('users').push({id: msg.author.id, items: []}).write();
+    msg.reply(`Your ID is : ${msg.author.id}\nCreated database`);
 }
 
-function add(args) {
-  db.get('users').find({id: msg.author.id}).get('items').push({Q: args[1]}).write();
-  msg.reply("Added " + args[1]);
+function add(msg, args) {
+
+    const userData = db.get('users').find({id: msg.author.id}).get('items'); 
+
+    if(!userData.value()) {
+        msg.reply("You are not registered yet!");
+    }
+    else {
+
+        let str = "";
+
+        for(let i = 1; i < args.length; i++) {
+            db.get('users').find({id: msg.author.id}).get('items').push({Q: args[i]}).write();
+            str += ` ${args[i]} `;
+        }
+
+        msg.reply("Added " + str);
+    }
+
+    return;
+}
+
+function display(msg) {
+    const data = db.get('users').find({id: msg.author.id}).get('items').value();
+
+    if(!data) {
+        msg.reply("You are not registered yet!");
+    }
+    else if(data.length === 0) {
+        msg.reply("No Questions, Please Try Some.");
+    }
+    else {
+        let str = "\n";
+        for(let i in data) {
+            str += `${Number(i) + 1} . ${data[i]['Q']}\n`;
+        }
+
+        msg.reply(str);
+    }
+    return;
 }
 
 bot.login(TOKEN);
